324a53c147c74a872d39793af16196d606e5c55c
zipit145 <gregbforney@gmail.com>
2018-08-23 15:48:24 -0600

    work on day 4

--- gregf.js
+++ gregf.js
@@ -2,8 +2,14 @@ var span = document.getElementsByClassName("close")[0];
 $(".jsEx").click(function() {
   $(".modal").removeClass('inactive');
 });
+$(".jsEx2").click(function() {
+  $(".modal2").removeClass('inactive');
+});
 $( ".close" ).click(function() {
   $(".modal").addClass('inactive');
+});
+$( ".close" ).click(function() {
+  $(".modal2").addClass('inactive');
 });
 $(".calculate").click(function() {
   var c1v1c2v2 = [
@@ -12,24 +18,20 @@ $(".calculate").click(function() {
     $(".c2").val(),
     $(".v2").val()
   ]
-  var c1 = $(".c1").val();
-  var v1 = $(".v1").val();
-  var c2 = $(".c2").val();
-  var v2 = $(".v2").val();
   switch (!!c1v1c2v2) {
-    case c1v1c2v2[0] !=="", c1v1c2v2[1] !=="", c1v1c2v2[2] !=="":
+    case c1v1c2v2[0] !=="" && c1v1c2v2[1] !=="" && c1v1c2v2[2] !=="":
       var result = $(".c1").val() * $(".v1").val() / $(".c2").val();
       $(".v2").val(result);
       break;
-    case c1v1c2v2[0] !=="", c1v1c2v2[1] !=="", c1v1c2v2[3] !=="":
+    case c1v1c2v2[0] !=="" && c1v1c2v2[1] !=="" && c1v1c2v2[3] !=="":
       var result = $(".c1").val() * $(".v1").val() / $(".v2").val();
       $(".c2").val(result);
       break;
-    case c1v1c2v2[0] !=="", c1v1c2v2[2] !=="", c1v1c2v2[3] !=="":
+    case c1v1c2v2[0] !=="" && c1v1c2v2[2] !=="" && c1v1c2v2[3] !=="":
       var result = $(".c2").val() * $(".v2").val() / $(".c1").val();
       $(".v1").val(result);
       break;
-    case c1v1c2v2[1] !=="", c1v1c2v2[2] !=="", c1v1c2v2[3] !=="":
+    case c1v1c2v2[1] !=="" && c1v1c2v2[2] !=="" && c1v1c2v2[3] !=="":
       var result = $(".c2").val() * $(".v2").val() / $(".v1").val();
       $(".c1").val(result);
       break;
@@ -50,6 +52,59 @@ $(".calculate").click(function() {
   //   $(".v1").val(result);
   // }
 });
+
+//Molarity calculator
+$( ".elements" ).change(function() {
+  $(".mw").val($(".elements").val());
+});
+
+  //calculate the answer
+
+$( ".calculatemw" ).click(function() {
+  var variables = [
+    $(".volume").val(),
+    $(".mass").val(),
+    $(".concentration").val(),
+    $(".mw").val(),
+  ]
+  var units = [
+    Math.pow(10,$(".volumeunits").val()),
+    Math.pow(10,$(".massunits").val()),
+    Math.pow(10,$(".concentrationunits").val()),
+    1,
+  ] 
+  switch (!!variables && !!units) {
+    case variables[0] !=="" && variables[2] !=="" && variables[3] !=="" && units[2] !=="":
+      var unit = units[2] * units[0];
+      var calc = variables[2] * variables[0] * variables[3];
+      //check for unit change on answer
+      var result = unit*calc;
+      if ( result > 10 || result < 0.1){
+        var exponent = result.toExponential().match(/e.*/);
+        $(".massunits").val(exponent[0].substring(1));
+        var result = calc;
+        $(".mass").val(result);
+      } else {
+        $(".massunits").val("0");
+        $(".mass").val(result);
+      }
+      break;
+    case variables[1] !=="" && variables[2] !=="" && variables[3] !=="":
+    alert("volume");
+      var calc = variables[1] / (variables[2] * variables[3]);
+      $(".volume").val(result);
+      break;
+    case variables[0] !=="" && variables[1] !=="" && variables[3] !=="":
+    alert("conc");
+      var calc = variables[1] / (variables[0] * variables[3]);
+      $(".concentration").val(result);
+      break;
+    default:
+      alert("default");
+  }
+});
+
+//old basic division
 // $(".calculate").click(function() {
 //   var result = $(".numerator").val() / $(".denominator").val();
 //   $(".answer").html("The answer is: ".concat(result));
